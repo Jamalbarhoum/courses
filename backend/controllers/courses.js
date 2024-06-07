828eab18e10ffc6d4168b177a6690cf547a8256e
Jamalbarhoum <jamallbarhoum@gmail.com>
2024-06-07 16:29:22 +0300

    get courses

--- backend/controllers/courses.js
+++ backend/controllers/courses.js
@@ -1,10 +1,11 @@
 const { pool } = require("../models/db");
 
+//! API =>  http://localhost:5000/courses {post}
 const CreateNewCourse = (req, res) => {
   const { name, description, image } = req.body;
   const teacher_id = req.token.user_id;
   const values = [name, description, image, teacher_id];
-  
+
   const query = `INSERT INTO courses (name,description,image,teacher_id) VALUES ($1,$2,$3,$4) RETURNING *`;
   pool
     .query(query, values)
@@ -23,8 +24,9 @@ const CreateNewCourse = (req, res) => {
       });
     });
 };
-const deleteCourse = (req, res) => {
 
+//! API =>  http://localhost:5000/courses/:id {delete}
+const deleteCourse = (req, res) => {
   const id_course_deleted = req.params.id;
   const teacher_id = req.token.user_id;
 
@@ -33,14 +35,14 @@ const deleteCourse = (req, res) => {
   pool
     .query(query)
     .then((result) => {
-        console.log(result);
-        if(!result.rowCount){
-            return res.status(404).json({
-                success:false,
-                message:"course is not exist",
-                result:result.rows
-            })
-        }
+      console.log(result);
+      if (!result.rowCount) {
+        return res.status(404).json({
+          success: false,
+          message: "course is not exist",
+          result: result.rows,
+        });
+      }
       return res.status(201).json({
         success: true,
         message: "course deleted successful",
@@ -56,4 +58,72 @@ const deleteCourse = (req, res) => {
     });
 };
 
-module.exports = { CreateNewCourse, deleteCourse };
+//! API =>  http://localhost:5000/courses/priv  {get}
+
+// All private courses in this teacher
+const allCoursesForOneTeacher = (req, res) => {
+  const teacher_id = req.token.user_id;
+  const query = `select * from courses where teacher_id=${teacher_id}`;
+  pool
+    .query(query)
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "all courses for 1 teacher",
+          result: result.rows,
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "all courses for 1 teacher",
+        result: result.rows,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        success: false,
+        message: "server error",
+        error: err,
+      });
+    });
+};
+
+//! API =>  http://localhost:5000/courses/  {get}
+
+// get all courses
+const allCourses = (req, res) => {
+  const query = `select * from courses`;
+  pool
+    .query(query)
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "all courses for 1 teacher",
+          result: result.rows,
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "all courses for 1 teacher",
+        result: result.rows,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        success: false,
+        message: "server error",
+        error: err,
+      });
+    });
+};
+
+module.exports = {
+  CreateNewCourse,
+  deleteCourse,
+  allCoursesForOneTeacher,
+  allCourses,
+};
--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,10 +1,12 @@
 const express = require("express")
-const {CreateNewCourse,deleteCourse} = require("../controllers/courses")
+const {CreateNewCourse,deleteCourse,allCoursesForOneTeacher,allCourses} = require("../controllers/courses")
 const authentication = require("../middlewares/authentication")
 const authorization = require("../middlewares/authorization")
 const coursesRouter = express.Router()
 
 coursesRouter.post("/",authentication,authorization("CreateNewCourse"),CreateNewCourse)
-coursesRouter.delete("/:id",authentication,authorization("CreateNewCourse"),deleteCourse)
+coursesRouter.delete("/:id",authentication,authorization("deleteCourse"),deleteCourse)
+coursesRouter.get("/priv",authentication,authorization("watchCourse"),allCoursesForOneTeacher)
+coursesRouter.get("/",authentication,authorization("watchCourse"),allCourses)
 
 module.exports = coursesRouter
